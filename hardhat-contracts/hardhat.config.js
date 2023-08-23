28687405afe80c965505a8843090133affc94629
0xDanki <typepuller@gmail.com>
2023-08-23 13:05:03 +0800

    frontend

--- hardhat-contracts/hardhat.config.js
+++ hardhat-contracts/hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+   "mantle-testnet": {
+       url: "https://rpc.ankr.com/mantle_testnet",
+     accounts: [process.env.PRIVATE_KEY], // Uses the private key from the .env file
+   }
+ }
 };
---
+++ koipond-nextjs/app/theme/themes.ts
@@ -0,0 +1,7 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+export const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
